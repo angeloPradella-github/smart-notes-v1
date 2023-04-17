c25ca72ce2de055c507f097eee15be653f19f3ea
angeloPradella-github <angelogpradella@gmail.com>
2023-04-17 18:34:18 +0200

    prova messa online 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/smart-notes-v1/" : "/",
+  base: "/smart-notes-v1/",
 });
